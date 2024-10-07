259191173417fcba9ee254571fb0deacdace78ac
Alexander Olofsson <alexander.olofsson@liu.se>
2024-10-07 15:41:43 +0200

    Make rubocop happier

--- lib/foreman_cement/configure_sentry.rb
+++ lib/foreman_cement/configure_sentry.rb
@@ -4,6 +4,7 @@ require 'sentry-ruby'
 require 'sentry-rails'
 require 'sentry-sidekiq'
 
+# rubocop:disable Metrics/BlockLength
 Sentry.init do |config|
   config.logger = Foreman::Logging.logger('foreman_cement')
   config.dsn = SETTINGS.with_indifferent_access['sentry_dsn']
@@ -11,7 +12,7 @@ Sentry.init do |config|
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
   if SETTINGS.with_indifferent_access['sentry_trace']
-    main_rate = \
+    main_rate =
       if SETTINGS.with_indifferent_access['sentry_trace'].is_a?(Hash)
         SETTINGS.with_indifferent_access.dig('sentry_trace', 'rate') || 0.5
       else
@@ -46,3 +47,4 @@ Sentry.init do |config|
   #   filter.filter(event.to_hash)
   # end
 end
+# rubocop:enable Metrics/BlockLength
